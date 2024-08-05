baeaaa28481b21e4160bf10aaba34303f548f73e
manzitresor <manzitre@gmail.com>
2024-08-05 12:01:58 +0200

    Add relative path to main page

--- pages/Host/Dashboard.jsx
+++ pages/Host/Dashboard.jsx
@@ -4,10 +4,7 @@ import {Link, Outlet} from "react-router-dom"
 export default function Dashboard() {
     return (
         <>
-        
-            <Link to="/host/income">Income</Link>
-            <Link to="/host/reviews">Reviews</Link>
-            <Outlet />
+        <h1> Dashboard</h1>
         </>
     )
 }
--- pages/Host/HostLayout.jsx
+++ pages/Host/HostLayout.jsx
@@ -3,9 +3,13 @@ import { Link, Outlet  } from "react-router-dom";
 export default function HostLayout() {
   return (
     <>
-     <Link to="/host/income">Income</Link>
-          <Link to="/host/reviews">Reviews</Link>
-     <Outlet />
+    <nav className="host-nav">
+      <Link to="/host">Dashboard</Link>
+      <Link to="/host/income">Income</Link>
+      <Link to="/host/reviews">Reviews</Link>
+    </nav>
+    <Outlet />
+    
     </>
   )
 }
